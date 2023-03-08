e79776dc02188073cfd0a8d17838ddbb9fd9e4a1
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-03-08 19:19:49 +0000

    Added comments

--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@
 // Created on: Sep 2023
 // This file contains the JS functions for index.html
 
+// Basic math
 function mathButtonClicked() {
   document.getElementById("add-math").innerHTML = "<p>1 + 3 = " + (1 + 3) + "</p>"
   document.getElementById("subtract-math").innerHTML = "<p>2 - 4 =  " + (2 - 4) + "</p>"  
   document.getElementById("multiply-math").innerHTML = "<p>3 * 5 = " + (3 * 4) + "</p>" 
   document.getElementById("divide-math").innerHTML = "<p>6 / 2 = " + (6 / 2) + "</p>"
   document.getElementById("exponent-math").innerHTML = "<p>2^3 + 1 = " + (2 ** 3 + 1) + "</p>"
+// Create notification after button is clicked
   document.getElementById("solved").innerHTML = "Solved!"
 }
