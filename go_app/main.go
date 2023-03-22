72cf5b1f86db0714876a9eb5a38aefbca5294ecf
Mohamad-Tanbari <124389317+Mohamad-Tanbari@users.noreply.github.com>
2023-03-22 01:29:02 +0000

    added space

--- go_app/main.go
+++ go_app/main.go
@@ -11,9 +11,9 @@ import (
 
 func main() {
 	fmt.Println("The dimentions of the rectangle are 5cm and 3cm")
-	fmt.Println("Area of rectangle: ", (5 * 3), "cm²")
-	fmt.Println("Perimeter of rectangle: ", (2 * (5 + 3)), "cm")
+	fmt.Println("Area of rectangle: ", (5 * 3), " cm²")
+	fmt.Println("Perimeter of rectangle: ", (2 * (5 + 3)), " cm")
 
 	// end of program
-	fmt.Println("\nDone")
+	fmt.Println("\nDone.")
 }
